169f0c60f97b86f7d63114e2d4a3aa87e809e5f1
tundeiness <tundeiness@yahoo.co.uk>
2020-10-25 14:34:23 +0100

    fix missing do in api namespace in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  
-  namespace :api, defaults: { format: :json }
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # We are going to list our resources here
     end

174adac4d4c46b4631626201e86d3e24322f1a6a
tundeiness <tundeiness@yahoo.co.uk>
2020-10-25 10:50:59 +0100

    configure Rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,4 +67,24 @@ RSpec.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+
+  RSpec.configure do |config|
+    
+    # add `FactoryBot` methods
+    config.include FactoryBot::Syntax::Methods
+  
+    # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+    config.before(:suite) do
+      DatabaseCleaner.clean_with(:truncation)
+      DatabaseCleaner.strategy = :transaction
+    end
+  
+    # start the transaction strategy as examples are run
+    config.around(:each) do |example|
+      DatabaseCleaner.cleaning do
+        example.run
+      end
+    end
+  end
+
 end
